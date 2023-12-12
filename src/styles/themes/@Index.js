00e5097fa7ede399242fad1184b6346228e74069
hyetjs <cherryade98@gmail.com>
2023-12-12 21:28:34 +0900

    공용 style-theme 생성

---
+++ src/styles/themes/@Index.js
@@ -0,0 +1,5 @@
+import color from './Color'
+import flexAlign from './FlexAlign'
+import fontSize from './FontSize'
+import fontWeight from './FontWeight'
+export { color, flexAlign, fontSize, fontWeight }
