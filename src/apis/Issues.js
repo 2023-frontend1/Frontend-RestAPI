6c25a2d83f8d9b05abf18808f626bd6bf66ccbf6
yunshin <2younsin@gmail.com>
2023-12-14 17:25:58 +0900

    feat: issue 데이터 불러오는 함수 작성

---
+++ src/apis/Issues.js
@@ -0,0 +1,27 @@
+import axiosInstance from './Base'
+
+/**
+ * @author 최혜선
+ *
+ * @param {number} page pagination 번호 입니다.
+ * @param {number} perPage 특정 페이지에서 불러올 issue 갯수 입니다.
+ * @param {"created" | "updated" | "comments"} sort 데이터 정렬 기준입니다.
+ */
+const getIssuesList = async (page = 1, perPage = 10, sort = 'created') => {
+	try {
+		const response = await axiosInstance.get(
+			`/repos/angular/angular-cli/issues`,
+			{
+				params: {
+					page: page,
+					per_page: perPage,
+					sort: sort,
+				},
+			}
+		)
+		console.log(response.data)
+	} catch (error) {
+		console.log(error)
+	}
+}
+export default getIssuesList
